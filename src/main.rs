400c0317a1f83d451bec3a7bfebc51b51e39e69d
Denton24646 <Denton24646@gmail.com>
2019-08-28 21:46:35 -0400

    feat: Pointer compiles

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,12 @@ use std::io::Write;
 fn main() {
     let point_x: &str = "0x50863AD64A87AE8A2FE83C1AF1A8403CB53F53E486D8511DAD8A04887E5B2352";
     let point_y: &str = "0x2CD470243453A299FA9E77237716103ABC11A1DF38855ED6F2EE187E9C582BA6";
-    println!("X = {}", *point_x);
-    println!("Y = {}", *point_y);
+   // println!("X = {}", *point_x);
+   // println!("Y = {}", *point_y);
 
     struct Point {
-        x: [u8; 32],
-        y: [u8; 32],
+        x: Vec<u8>,
+        y: Vec<u8>,
     };
 
     pub trait Concat {
@@ -19,8 +19,8 @@ fn main() {
         // concatenate x and y points
         // add one byte prefix equal to 4 per convention
         fn concatenate(&self) -> [u8; 65] {
-            let mut z: [u8; 65];
-            z[0] = 4;
+            let mut z: [u8; 65] = [0; 65];
+            (&mut z[0..1]).write(&[b'4']).unwrap();
             (&mut z[1..]).write(&self.x[..]).unwrap();
             (&mut z[33..]).write(&self.y[..]).unwrap();
             z
@@ -28,12 +28,12 @@ fn main() {
     };
 
     let p = Point {
-        x: *point_x.as_bytes(),
-        y: *point_y.as_bytes(),
+        x: (*point_x.as_bytes()).to_vec(),
+        y: (*point_y.as_bytes()).to_vec(),
     };
 
     let result:[u8; 65] = p.concatenate();
-    println!("result = {}", result);
+    //println!("result = {}", result);
 }
 
 
